6447c2831d0b3ea4c41eae68c084e589b0eb87e0
Daiki037 <daniel037@hotmail.es>
2025-12-03 23:35:02 -0500

    .

--- src/api/endPoints.js
+++ src/api/endPoints.js
@@ -1,34 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-// Controladores disponibles
-const { login } = require('../controllers/loginController')
-const {
-  forgotPassword,
-  resetPassword,
-  verifyResetToken,
-} = require('../controllers/forgotPasswordController')
-const usuarioController = require('../controllers/usuarioController')
 const sqlScriptsController = require('../controllers/sqlScriptsController')
 const powerBIController = require('../controllers/powerBIController')
 const imagesController = require('../controllers/imagesController')
 const scriptExecutorController = require('../controllers/scriptExecutorController')
 
-// Rutas de autenticación
-router.post('/login', login)
-
-// Rutas para recuperación de contraseña
-router.post('/forgot-password', forgotPassword) // Enviar email de recuperación
-router.post('/reset-password', resetPassword) // Restablecer contraseña
-router.get('/verify-reset-token/:token', verifyResetToken) // Verificar token de reset
-
-// Rutas para el controlador de usuarios
-router.get('/usuarios', usuarioController.getAllUsuarios)
-router.get('/usuarios/:id', usuarioController.getUsuarioById)
-router.delete('/usuarios/:id', usuarioController.deleteUsuario)
-router.post('/usuarios', usuarioController.createUsuario)
-router.put('/usuarios/:id', usuarioController.updateUsuario)
-router.put('/usuarios/:id/activar', usuarioController.updateUsuarioActivo)
 
 // Rutas para scripts SQL
 router.get('/sql-scripts', sqlScriptsController.getScriptsList)
@@ -47,6 +24,6 @@ router.post('/execute-script/:filename', scriptExecutorController.executeScript)
 router.post('/execute-all-scripts', scriptExecutorController.executeAllScripts)
 
 
-
+   
 
 module.exports = router
--- src/dtos/rolUsuario.dto.js
+++
@@ -1,8 +0,0 @@
-class RolUsuarioDTO {
-    constructor(id_rolusuario, nom_rolusuario) {
-      this.id_rolusuario = id_rolusuario;
-      this.nom_rolusuario = nom_rolusuario;
-    }
-  }
-  
-  module.exports = RolUsuarioDTO;
